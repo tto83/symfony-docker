48b8643db92eb399bcb18af97416d6e4e41da07d
Tomek <t.j.tomasz@gmail.com>
2022-11-15 08:02:20 +0100

    Practice addins, add Twig, add questions and answers display

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,10 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
@@ -20,10 +21,16 @@ class QuestionController
      */
     public function show($slug)
     {
-        return new Response(
-            sprintf(
-                'Feature to show a question "%s" ', ucwords(str_replace('-',' ',$slug))
-            )
-        );
+        $answers = [
+          'Have you tried to turn it off an on again?',
+          'It works for me',
+          "I think it's better now"
+        ];
+
+        return $this->render('question/show.html.twig', [
+            'question'=>ucwords(str_replace('-',' ',$slug)),
+            'answers'=>$answers
+
+        ]);
     }
 }
