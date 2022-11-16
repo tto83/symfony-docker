991a591806a483a858444b93b8c2201a9325f429
Tomek <t.j.tomasz@gmail.com>
2022-11-16 16:46:22 +0100

    Add markdown bundle configuration file. Change markdown parser to light.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -34,13 +34,12 @@ class QuestionController extends AbstractController
             "I think it's `purrfect`"
         ];
 
-        dump($this);
-
-
         $questionText ='I\'ve been turned into a cat, any thoughts on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
         $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
             return $markdownParser->transformMarkdown($questionText);
         });
+
+        dd($markdownParser);
 
         return $this->render('question/show.html.twig', [
             'question'=>ucwords(str_replace('-',' ',$slug)),
